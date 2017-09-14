2ac3600d1b5491e51219a6d2ec3d0d5d83238197
John Desrosiers <johndrunner@yahoo.com>
2017-09-14 16:40:26 -0700

    BasicCard is scope safe

--- BasicCard.js
+++ BasicCard.js
@@ -1,7 +1,11 @@
 // create BasicCard constructor
 var BasicCard = function(front, back) {
-    this.front = front;
-    this.back = back;
+    if ( this instanceof BasicCard ) {
+        this.front = front;
+        this.back = back;
+    } else {
+        return new BasicCard(front, back);
+    }
 };
 // export BasicCard constructor
 module.exports = BasicCard;
