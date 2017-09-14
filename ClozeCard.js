13f827862933e2d6b10759e0c74b1cd6c7a2e03e
John Desrosiers <johndrunner@yahoo.com>
2017-09-14 16:27:37 -0700

    add ClozeCard constructor

---
+++ ClozeCard.js
@@ -0,0 +1,25 @@
+// Constructor for ClozeCard class
+var ClozeCard = function(text, cloze) {
+    this.cloze = cloze;
+    this.fullText = text;
+
+    // if cloze is not found in text...
+    if ( text.indexOf(cloze) === -1 ) {
+        // ... throw an error
+        throw new InvalidClozeException(text, cloze);
+    }
+    // replace cloze with placeholder
+    this.partial = text.replace(cloze, "...");
+};
+
+// Constructor for an Invalid Cloze Exception error
+function InvalidClozeException(text, cloze) {
+    this.type = "CLOZE_ERROR";
+    this.text = text;
+    this.cloze = cloze;
+    this.message = "Invalid ClozeCard parameters. Cloze parameter must be found in text parameter.";
+    this.toString = function() {
+        return this.type + ":" + this.message + "\n\"" + this.cloze + "\" was not found in \"" + this.text + "\"";
+    }
+}
+module.exports = ClozeCard;
