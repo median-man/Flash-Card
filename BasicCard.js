c8bebaa341370366ad3fd384abd02c507bf3f0fb
John Desrosiers <johndrunner@yahoo.com>
2017-09-14 15:53:14 -0700

    added BasicCard.js

---
+++ BasicCard.js
@@ -0,0 +1,7 @@
+// create BasicCard constructor
+var BasicCard = function(front, back) {
+    this.front = front;
+    this.back = back;
+};
+// export BasicCard constructor
+module.exports = BasicCard;
