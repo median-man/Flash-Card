d75efc545f8fa42816c58d14ee443180202d8f69
John Desrosiers <johndrunner@yahoo.com>
2017-09-14 16:39:16 -0700

    made ClozeCard scope safe

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,15 +1,19 @@
 // Constructor for ClozeCard class
 var ClozeCard = function(text, cloze) {
-    this.cloze = cloze;
-    this.fullText = text;
+    if ( this instanceof ClozeCard ) {
+        this.cloze = cloze;
+        this.fullText = text;
 
-    // if cloze is not found in text...
-    if ( text.indexOf(cloze) === -1 ) {
-        // ... throw an error
-        throw new InvalidClozeException(text, cloze);
+        // if cloze is not found in text...
+        if ( text.indexOf(cloze) === -1 ) {
+            // ... throw an error
+            throw new InvalidClozeException(text, cloze);
+        }
+        // replace cloze with placeholder
+        this.partial = text.replace(cloze, "...");
+    } else {
+        return new ClozeCard(text, cloze);
     }
-    // replace cloze with placeholder
-    this.partial = text.replace(cloze, "...");
 };
 
 // Constructor for an Invalid Cloze Exception error
