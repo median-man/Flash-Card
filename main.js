14c8060cc501e854f160ade5ef772db1d81a0c08
John Desrosiers <johndrunner@yahoo.com>
2017-09-15 06:38:06 -0700

    added input handling and function stubs

--- main.js
+++ main.js
@@ -7,23 +7,76 @@ const ClozeCard = require("./ClozeCard");
 /* 
 *   Globals
  */
+// constants for operators
 const opAdd = "add";
 const opReview = "review";
+
+// constants for card type
+const typeCloze = "cloze";
+const typeBasic = "basic";
+
 
 /* 
 *   Evaluate parameters
  */
 var operation = process.argv[2]; // add or review cards
 var args = process.argv.slice(3);
+
+// add a new card to storage from input
 if ( operation === opAdd ) {
-    // add card data passed in parameters
+    // add card data from args
+    var cardType = args[0];
+    var newCard;
+    if ( cardType === typeBasic ) {
+        // create a new basic card
+        newCard = new BasicCard(args[1], args[2]);
+        newCard.type = typeBasic;
+    }
+    else if ( cardType === typeCloze ) {
+        // create a new cloze card
+        newCard = new ClozeCard(args[1], args[2]);
+        newCard.type = typeCloze;
+    }
+    else {
+        // invalid type
+        throw new InvalidArgumentException(type + " is not a valid card type.");
+    }
+    // add card to storage
+    addCard(JSON.stringify(newCard))
+        .then(
+            function(cardData) {
+                // log result
+                console.log("Added card:\n", cardData);
+            }
+        )
+        .catch(
+            function(reason) {
+                // log reason for failure
+                console.log("Failed to add card:\n", reason);
+            }
+        );
+
 }
+// return array of cards from storage
 else if ( operation === opReview ) {
-    // return array of cards
+    // get the cards from storage
+    getCards()
+        .then(
+            function(cardData) {
+                // log the cards array
+                console.log("Successfully retrieved cards.\n" + JSON.parse(cardData));
+            }
+        )
+        .catch(
+            function(reason) {
+                // log reason for failure to retrive cards
+                console.log("Failed to get the cards.\n" + reason);
+            }
+        );
 }
 else {
     // invalid operation
-    throw (operation + " is not a valid operation.");
+    throw new InvalidArgumentException(operation + " is not a valid operation.");
 }
 
 // TODO: this function is a placeholder. needs logic to store the card data.
@@ -33,8 +86,7 @@ function addCard(cardData) {
     return new Promise(
         function(resolve, reject) {
             try {
-                // add the card data to storage
-
+                // TODO: add the card data to storage
                 // pass card to the callback
                 resolve(cardData);
             }
@@ -62,3 +114,17 @@ function getCards() {
         }
     );
 }
+
+/* 
+* Exception Classes
+ */
+function InvalidArgumentException(message) {
+    this.message = message;
+    this.name = "InvalidInputException";
+}
+
+
+
+/* 
+*   Tests
+*/
