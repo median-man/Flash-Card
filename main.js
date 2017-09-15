c83d56ae12ee3c7efb8fcc5f392ef8abfeda6bd2
John Desrosiers <johndrunner@yahoo.com>
2017-09-15 05:45:08 -0700

    main.js setup to accept arguments

--- main.js
+++ main.js
@@ -1,125 +1,64 @@
 /* 
 *   Import Modules
  */
-const inquirer = require("inquirer");
 const BasicCard = require("./BasicCard");
 const ClozeCard = require("./ClozeCard");
-const CardStore = require("./CardStore");
 
-// get a global instance of CardStore
-var myStore = CardStore("cards.json");
+/* 
+*   Globals
+ */
+const opAdd = "add";
+const opReview = "review";
 
-// constants for commands
-const cmdAddCards = "add";
-const cmdReview = "review";
-const cmdQuit = "quit";
-
-function run() {
-    // prompt user for command
-    inquirer.prompt({
-        name: 'command',
-        type: 'list',
-        message: "Add cards or review?",
-        choices: [cmdAddCards, cmdReview, cmdQuit]
-    })
-    .then(function(answers) {
-        if ( answers.command === cmdAddCards ) {
-            addCard();
-        } else if ( answers.command === cmdReview ) {
-            console.log("review cards chosen");
-        } else if ( answers.command === cmdQuit ) {
-            process.exit();
-        }
-    });
+/* 
+*   Evaluate parameters
+ */
+var operation = process.argv[2]; // add or review cards
+var args = process.argv.slice(3);
+if ( operation === opAdd ) {
+    // add card data passed in parameters
+}
+else if ( operation === opReview ) {
+    // return array of cards
+}
+else {
+    // invalid operation
+    throw (operation + " is not a valid operation.");
 }
 
-// prompts user for input and adds card to store
-function addCard() {
-    const typeBasic = 'basic';
-    const typeCloze = 'cloze';
-    inquirer.prompt(
-        [
-            {
-                name: 'cardType',
-                type: 'list',
-                message: 'Choose type of card to add:',
-                choices: [typeBasic, typeCloze]
-            },
-            {
-                name: 'front',
-                type: 'input',
-                message: 'Enter front text:',
-                when: function(answers) { return answers.cardType === typeBasic }
-            },
-            {
-                name: 'back',
-                type: 'input',
-                message: 'Enter back text:',
-                when: function(answers) { return answers.cardType === typeBasic }
-            },
-            {
-                name: 'fullText',
-                type: 'input',
-                message: 'Enter full text for cloze card:',
-                when: function(answers) { return answers.cardType === typeCloze }
-            },
-            {
-                name: 'clozeText',
-                type: 'input',
-                message: 'Enter text to delete for cloze card:',
-                when: function(answers) { return answers.cardType === typeCloze }
+// TODO: this function is a placeholder. needs logic to store the card data.
+// Returns a promise. Adds a new card to storage passing the new card
+// to the returned promise.
+function addCard(cardData) {
+    return new Promise(
+        function(resolve, reject) {
+            try {
+                // add the card data to storage
+
+                // pass card to the callback
+                resolve(cardData);
             }
-        ]
-    )
-    .then(
-        function(answers) {
-            var cardsArray;
-            var newCard;
-            if ( answers.cardType === typeBasic ) {
-                // get a new basic card
-                newCard = new BasicCard(answers.front, answers.back);
+            catch (reason) {
+                // return failure message to callback
+                reject(reason);
             }
-            else if ( answers.cardType === typeCloze ) {
-                newCard = new ClozeCard(answers.fullText, answers.clozeText);
-            }
-            // get data from the store
-            myStore.getData()
-            .then(
-                function(data) {
-                    cardsArray = data;
-                    // add new card to data
-                    cardsArray.push(newCard);
-                    // store data with the new card
-                    myStore.storeData(cardsArray);
-                    // go back to starting prompt
-                    run();
-                }
-            )
-            .catch(
-                function(err) {
-                    console.log("Error adding card:\n", err);
-                }
-            );
-        }
-    )
-    // error getting user input
-    .catch(
-        function(err) {
-            console.log(err);
         }
     );
 }
 
-// Retrieves cards from store and interactively displays them to the user
-function reviewCards() {
-    // get the cards
-    // for each card in cards
-        // show front or text
-        // wait for user to input flip card
-        // flip the card
-        // wait for user to go to next card
-        // next card
+// Returns a promise which is passed an array of card objects.
+// The array contains all cards in storage.
+function getCards() {
+    return new Promise(
+        function(resolve, reject) {
+            try {
+                var cards = [];
+                // TODO: get the cards from storage
+                resolve(cards);
+            }
+            catch (reason) {
+                reject(reason);
+            }
+        }
+    );
 }
-
-// start the application
-run();
