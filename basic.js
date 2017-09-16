d287bc6cca32f476f9b6fe40aae87252f5f0f7c7
John Desrosiers <johndrunner@yahoo.com>
2017-09-16 08:02:45 -0700

    added files for basic card cli

---
+++ basic.js
@@ -0,0 +1,20 @@
+/* 
+*   CLI for reviewing basic flash cards
+ */
+
+ /* 
+ *  Import Modules 
+ */
+const fs = require("fs");
+const inquirer = require("inquirer");
+const cards = require("./basic.json");
+
+// get cards from file storage
+var cards
+// get card
+// display the card prompt
+// compare input to answer after user enters input
+// build result string to display to the user
+// display result string
+// if there are more cards ...
+// show the next card
