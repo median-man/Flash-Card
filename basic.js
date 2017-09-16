2b884c238e254e5a7922c98139f88ea1626c0457
John Desrosiers <johndrunner@yahoo.com>
2017-09-16 08:51:13 -0700

    prompt one card

--- basic.js
+++ basic.js
@@ -7,14 +7,37 @@
  */
 const fs = require("fs");
 const inquirer = require("inquirer");
-const cards = require("./basic.json");
+const BasicCard = require("./BasicCard");
+const cards = require("./basic.json"); // array of flash card objects
 
-// get cards from file storage
-var cards
 // get card
+var card = new BasicCard(cards[0].front, cards[0].back);
 // display the card prompt
-// compare input to answer after user enters input
-// build result string to display to the user
-// display result string
+inquirer.prompt(
+    {
+        name: "userAnswer",
+        type: "input",
+        message: card.front + "\nAnswer: "
+    }
+).then(
+    function(answers) {
+        // build result string to display to the user
+        var resultMsg;
+        // compare input to answer after user enters input
+        if ( answers.userAnswer === card.back ) {
+            resultMsg = "You answered correctly!";
+        } else {
+            resultMsg = "Incorrect answer. The correct answer is \"" + 
+                card.back + "\".";
+        }
+        resultMsg += "\n--------------------------------------------------\n";
+        // display result string
+        console.log(resultMsg);
+    }
+).catch(
+    function(reason) {
+        console.log(reason);
+    }
+);
 // if there are more cards ...
 // show the next card
