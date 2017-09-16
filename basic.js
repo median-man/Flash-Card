4ceab89f3cd051b5abaf766db6b2708763b65f56
John Desrosiers <johndrunner@yahoo.com>
2017-09-16 09:13:21 -0700

    runs all cards

--- basic.js
+++ basic.js
@@ -10,34 +10,53 @@ const inquirer = require("inquirer");
 const BasicCard = require("./BasicCard");
 const cards = require("./basic.json"); // array of flash card objects
 
-// get card
-var card = new BasicCard(cards[0].front, cards[0].back);
-// display the card prompt
-inquirer.prompt(
-    {
-        name: "userAnswer",
-        type: "input",
-        message: card.front + "\nAnswer: "
-    }
-).then(
-    function(answers) {
-        // build result string to display to the user
-        var resultMsg;
-        // compare input to answer after user enters input
-        if ( answers.userAnswer === card.back ) {
-            resultMsg = "You answered correctly!";
-        } else {
-            resultMsg = "Incorrect answer. The correct answer is \"" + 
-                card.back + "\".";
+/* 
+* Globals
+ */
+var cardIndex = 0;
+
+// Runs the app until all cards have been reviewed by the user.
+function run() {
+    // get instance of BasicCard
+    var basicCard = new BasicCard(
+        cards[cardIndex].front, 
+        cards[cardIndex].back
+    );
+    // display the card prompt
+    inquirer.prompt(
+        {
+            name: "userAnswer",
+            type: "input",
+            message: basicCard.front + "\nAnswer: "
         }
-        resultMsg += "\n--------------------------------------------------\n";
-        // display result string
-        console.log(resultMsg);
-    }
-).catch(
-    function(reason) {
-        console.log(reason);
-    }
-);
-// if there are more cards ...
-// show the next card
+    ).then(
+        function(answers) {
+            // build result string to display to the user
+            var resultMsg;
+            // compare input to answer after user enters input
+            if ( answers.userAnswer === basicCard.back ) {
+                resultMsg = "You answered correctly!";
+            } else {
+                resultMsg = "Incorrect answer. The correct answer is \"" + 
+                    basicCard.back + "\".";
+            }
+            resultMsg += "\n--------------------------------------------------\n";
+            // display result string
+            console.log(resultMsg);
+            
+            // increment cardIndex
+            cardIndex++;
+            // run the next card if there are more cards
+            if ( cardIndex < cards.length ) {
+                run();
+            }
+        }
+    ).catch(
+        function(reason) {
+            console.log(reason);
+        }
+    );
+}
+
+// start the app
+run();
